ab13b69ec7655c1bbd3184fafc6af80626ca589d
Ilker Yildirim <111722018+ITSIlkerSE@users.noreply.github.com>
2022-10-04 12:55:19 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> BBBB to reload.
-        </p>
+          Edit <code>src/App.tsx</code>
         <a
           className="App-link"
           href="https://reactjs.org"
