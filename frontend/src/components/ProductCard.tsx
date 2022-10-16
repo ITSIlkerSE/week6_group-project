fe41f005214b62e26a60535f4a56aa5e08cea5ad
ryan shade <kyunoske@gmail.com>
2022-10-16 14:10:31 +0200

    add stars, fix text length

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -8,14 +8,18 @@ type ProductCardProps = {
 
 function ProductCard(props: ProductCardProps) {
 
-    const [rating, setRating] = useState(0)
+    let golden = [];
+    for(let i = 0; i < props.product.rating; i++) {
+        golden.push(<i className="fa-solid fa-star" key={i}></i>)
+    }
+
     return (
         <div className="card">
             <img src={props.product.image} className="card-img-top" style={{height: "190px"}}/>
                 <div className="card-body">
                     <h5 className="card-title"><strong>{props.product.name}</strong></h5>
                     <p className="list-group-item price">${props.product.price}</p>
-                    <p className="list-group-item">{props.product.rating}</p>
+                    <p style={{color: "#E6C700"}}>{golden}</p>
                 </div>
 
             <div className="gap-2 d-md-flex justify-content-md-center buttons">
