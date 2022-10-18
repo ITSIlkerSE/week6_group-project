b5aa18b1dae7c74265dc496a9ba2ae829e7a7b5e
ITSIlkerSE <itsilkerse@gmail.com>
2022-10-18 17:48:17 +0200

    Add controller integration tests

--- backend/src/test/java/com/example/backend/controller/MyWardrobeControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/MyWardrobeControllerTest.java
@@ -3,7 +3,6 @@ package com.example.backend.controller;
 import com.example.backend.model.MyWardrobeModel;
 import com.example.backend.repo.MyWardrobeRepo;
 import com.example.backend.service.IdService;
-import com.example.backend.service.MyWardrobeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,17 +13,114 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.http.RequestEntity.post;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
 
 class MyWardrobeControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
 
+    @Autowired
+    private MyWardrobeRepo myWardrobeRepo;
+
+    @MockBean
+    private IdService idService;
+
+    @Test
+    void addProduct() throws Exception {
+
+        //GIVEN
+        when(idService.generateId()).thenReturn("1");
+
+        String requestBody = """
+                {
+                        
+                        "name": "jacke",
+                        "image": "bild",
+                        "size" : "xl",
+                        "description":"schön",
+                        "price": "25",
+                        "category": "top",
+                        "brand": "nike",
+                        "rating": 5,
+                        "inStock": true
+                }
+                """;
+
+        String expectedJSON = """
+                {
+                        "id":"1",
+                        "name": "jacke",
+                        "image": "bild",
+                        "size" : "xl",
+                        "description":"schön",
+                        "price": "25",
+                        "category": "top",
+                        "brand": "nike",
+                        "rating": 5,
+                        "inStock": true
+                }
+                """;
+
+        //WHEN+THEN
+        mockMvc.perform(
+                        post("/api/wardrobe")
+                                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJSON));
+
+    }
+
+
+    @Test
+    void getAllMovies() throws Exception {
+        //GIVEN
+        myWardrobeRepo.save(new MyWardrobeModel("1", "jacke", "bild", "xl", "schön", "25", "top", "nike", 5, true));
+        myWardrobeRepo.save(new MyWardrobeModel("2", "jackee", "bild", "xl", "schön", "25", "top", "nike", 5, true));
+
+
+        //WHEN &THEN
+        String expectedJSON = """
+                [
+                    {
+                        "id":"1",
+                        "name": "jacke",
+                        "image": "bild",
+                        "size" : "xl",
+                        "description":"schön",
+                        "price": "25",
+                        "category": "top",
+                        "brand": "nike",
+                        "rating": 5,
+                        "inStock": true
+        
+                    },
+                    {
+                        "id":"2",
+                        "name": "jackee",
+                        "image": "bild",
+                        "size" : "xl",
+                        "description":"schön",
+                        "price": "25",
+                        "category": "top",
+                        "brand": "nike",
+                        "rating": 5,
+                        "inStock": true
+                    }
+                ]
+                """;
+        //WHEN+THEN
+        mockMvc.perform(get("/api/wardrobe"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJSON));
+
+    }
 }
